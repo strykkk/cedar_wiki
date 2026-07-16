e0d788ac719f85bfcc24f2346a6a5333529ced29
strykkk <119400087+strykkk@users.noreply.github.com>
2026-07-16 16:30:06 +0200

    marta + irmana + fixes

---
+++ character/Marta/wiki.js
@@ -0,0 +1,61 @@
+let CH_base = document.getElementById("1");
+let CH_Full = document.getElementById("2");
+let CH_img = document.getElementById("3");
+let CH_ALT = document.getElementById("5");
+let CH_ALT_FULL = document.getElementById("6");
+
+let SKILL_ACT_but = document.getElementById("10");
+let SKILL_P1_but = document.getElementById("11");
+let SKILL_P2_but = document.getElementById("12");
+
+let SKILL_ACT_TAB = document.getElementById("20");
+let SKILL_P1_TAB = document.getElementById("21");
+let SKILL_P2_TAB = document.getElementById("22");
+
+
+console.log(CH_img)
+
+CH_base.addEventListener("click", function (e) {
+    document.getElementById("3").style.backgroundImage = "url('IMG.png')";
+
+})
+
+CH_Full.addEventListener("click", function (e) {
+    document.getElementById("3").style.backgroundImage = "url('IMG2.png')";
+
+})
+
+
+CH_ALT.addEventListener("click", function (e) {
+    document.getElementById("3").style.backgroundImage = "url('IMG4.png')";
+
+})
+
+CH_ALT_FULL.addEventListener("click", function (e) {
+    document.getElementById("3").style.backgroundImage = "url('IMG5.png')";
+
+})
+
+SKILL_ACT_but.addEventListener("click", function (e) {
+    console.log(CH_img)
+    SKILL_ACT_TAB.style.display = 'table';
+    SKILL_P1_TAB.style.display = "none";
+    SKILL_P2_TAB.style.display = 'none'
+
+})
+
+SKILL_P1_but.addEventListener("click", function (e) {
+    console.log(CH_img)
+    SKILL_ACT_TAB.style.display = 'none';
+    SKILL_P1_TAB.style.display = 'table';
+    SKILL_P2_TAB.style.display = 'none'
+
+})
+
+SKILL_P2_but.addEventListener("click", function (e) {
+    console.log(CH_img)
+    SKILL_ACT_TAB.style.display = 'none';
+    SKILL_P1_TAB.style.display = "none";
+    SKILL_P2_TAB.style.display = 'table'
+
+})
