8b33e4f5b5a4e121567d61cc127ea17427b9a545
strykkk <119400087+strykkk@users.noreply.github.com>
2026-07-23 19:43:10 +0200

    natsuki + ordering

---
+++ wiki.js
@@ -0,0 +1,8 @@
+function onPageReady() {
+  new Tablesort(document.getElementById('JDR'));
+  new Tablesort(document.getElementById('Ruvila'));
+  new Tablesort(document.getElementById('Entent'));
+  new Tablesort(document.getElementById('Zarab'));
+}
+
+document.addEventListener('DOMContentLoaded', onPageReady, false);
