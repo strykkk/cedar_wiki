aaac7514273cee3c55fa3ec6ee1c2c16d87d8b45
strykkk <119400087+strykkk@users.noreply.github.com>
2026-08-10 22:18:30 +0200

    e

--- character/Character.js
+++ character/Character.js
@@ -1,6 +1,9 @@
 let CH_base = document.getElementById("1");
 let CH_Full = document.getElementById("2");
 let CH_img = document.getElementById("3");
+let CH_BAT = document.getElementById("4");
+let CH_ALT = document.getElementById("5");
+let CH_ALT_FULL = document.getElementById("6");
 
 let SKILL_ACT_but = document.getElementById("10");
 let SKILL_P1_but = document.getElementById("11");
@@ -12,16 +15,6 @@ let SKILL_P2_TAB = document.getElementById("22");
 
 
 console.log(CH_img)
-
-CH_base.addEventListener("click", function (e) {
-    document.getElementById("3").style.backgroundImage = "url('IMG.png')";
-
-})
-
-CH_Full.addEventListener("click", function (e) {
-    document.getElementById("3").style.backgroundImage = "url('IMG2.png')";
-
-})
 
 SKILL_ACT_but.addEventListener("click", function (e) {
     console.log(CH_img)
@@ -46,3 +39,26 @@ SKILL_P2_but.addEventListener("click", function (e) {
     SKILL_P2_TAB.style.display = 'table'
 
 })
+
+CH_base.addEventListener("click", function (e) {
+    document.getElementById("3").style.backgroundImage = "url('IMG.png')";
+
+})
+
+CH_Full.addEventListener("click", function (e) {
+    document.getElementById("3").style.backgroundImage = "url('IMG2.png')";
+
+})
+
+
+CH_ALT.addEventListener("click", function (e) {
+    document.getElementById("3").style.backgroundImage = "url('IMG4.png')";
+
+})
+
+CH_ALT_FULL.addEventListener("click", function (e) {
+    document.getElementById("3").style.backgroundImage = "url('IMG5.png')";
+
+})
+
+
