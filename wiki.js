f2af17f3376698b1714a02ed6e6697cb2b3d32d4
strykkk <119400087+strykkk@users.noreply.github.com>
2026-08-15 15:57:30 +0200

    entente

--- wiki.js
+++ wiki.js
@@ -1,7 +1,7 @@
 function onPageReady() {
   new Tablesort(document.getElementById('JDR'));
   new Tablesort(document.getElementById('Ruvila'));
-  new Tablesort(document.getElementById('Entent'));
+  new Tablesort(document.getElementById('Entente'));
   new Tablesort(document.getElementById('Zarab'));
   new Tablesort(document.getElementById('ODR'));
   new Tablesort(document.getElementById('OTHER'));
